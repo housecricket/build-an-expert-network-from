dd593fa3f3328619df860ee35c7c69654dce1560
tarastudio <taragame68@gmail.com>
2019-06-21 07:40:36 +0700

    no message

--- build-an-expert-network/python/expert_network.py
+++ build-an-expert-network/python/expert_network.py
@@ -1,38 +1,6 @@
 from collections import Counter
 from collections import defaultdict
-
-experts = [
-    {"id": 0, "name": "Bill Gates"},
-    {"id": 1, "name": "Mark Zuckerberg"},
-    {"id": 2, "name": "Steve Jobs"},
-    {"id": 3, "name": "Jeff Bezos"},
-    {"id": 4, "name": "Ha Dong Nguyen"},
-    {"id": 5, "name": "Trung Anh Dang"},
-    {"id": 6, "name": "Sundar Pichai"},
-    {"id": 7, "name": "Larry Page"},
-    {"id": 8, "name": "Sergey Brin"},
-    {"id": 9, "name": "Tim Cook"}
-]
-
-interests = [
-    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
-    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
-    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
-    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
-    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
-    (3, "statistics"), (3, "regression"), (3, "probability"),
-    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
-    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
-    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
-    (6, "probability"), (6, "mathematics"), (6, "theory"),
-    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
-    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
-    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
-    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
-]
-
-friendship_pairs = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
-                    (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
+from data import *
 
 # Initialize the dict with an empty list for each expert id:
 friendships = {expert["id"]: [] for expert in experts}
@@ -88,7 +56,8 @@ def friends_of_friends(expert):
         and foaf_id not in friendships[expert_id]  
     )
 
-# print(friends_of_friends(experts[1]))               # Counter({0: 2, 5: 1})
+# print(friends_of_friends(experts[1]))               
+# Counter({0: 2, 5: 1})
 
 #Find the ids of all users who like the target interest.
 def data_scientists_who_like(target_interest):
@@ -98,13 +67,13 @@ def data_scientists_who_like(target_interest):
 
    
 
-# Keys are interests, values are lists of user_ids with that interest
+# Keys are interests, values are lists of expert_id with that interest
 expert_ids_by_interest = defaultdict(list)
 
 for expert_id, interest in interests:
     expert_ids_by_interest[interest].append(expert_id)
 
-# Keys are user_ids, values are lists of interests for that expert_id.
+# Keys are expert_ids, values are lists of interests for that expert_id.
 interests_by_expert_id = defaultdict(list)
 
 for expert_id, interest in interests:
@@ -118,4 +87,26 @@ def most_common_interests_with(expert):
         if interested_expert_id != expert["id"]
     )
 
-print(most_common_interests_with(experts[1]))
+# print(most_common_interests_with(experts[1]))
+
+# Keys are years, values are lists of the salaries for each tenure.
+income_by_tenure = defaultdict(list)
+
+for income, tenure in incomes_and_tenures:
+    income_by_tenure[tenure].append(income)
+
+# Keys are years, each value is average salary for that tenure.
+average_income_by_tenure = {
+    tenure: sum(incomes) / len(incomes)
+    for tenure, incomes in income_by_tenure.items()
+}
+
+# print(average_income_by_tenure)
+
+def tenure_bucket(tenure):
+    if tenure < 2:
+        return "less than two"
+    elif tenure < 5:
+        return "between two and five"
+    else:
+        return "more than five"
---
+++ build-an-expert-network/python/visualizing_data.py
@@ -0,0 +1,25 @@
+from matplotlib import pyplot as plt
+from data import *
+
+# create a line chart, years on x-axis, incomes on y-axis
+years = []
+incomes = []
+labels = []
+for i in range(0, 9):
+    years.append(incomes_and_tenures[i][1])
+    incomes.append(incomes_and_tenures[i][0])
+    labels.append(experts[i]["name"])
+
+plt.scatter(years, incomes, color='green')
+
+# label each point
+for label, years_count, incomes_count in zip(labels, years, incomes):
+    plt.annotate(label,
+        xy=(years_count, incomes_count), # Put the label with its point
+        xytext=(5, -5),                  # but slightly offset
+        textcoords='offset points')
+
+plt.title("Income by Years Experience")
+plt.xlabel("Year")
+plt.ylabel("Income")
+plt.show()
